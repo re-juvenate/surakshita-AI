f05af6e1de1cb22e650ce516b8e1e4dc26dcdefe
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-09-08 19:03:27 +0530

    stitching two images

---
+++ training_pipeline/image_operations.py
@@ -0,0 +1,30 @@
+# from PIL import Image
+
+# def merge_images(file1, file2):
+#     """Merge two images into one, displayed side by side
+#     :param file1: path to first image file
+#     :param file2: path to second image file
+#     :return: the merged Image object
+#     """
+#     image1 = Image.open(file1)
+#     image2 = Image.open(file2)
+
+#     (width1, height1) = image1.size
+#     (width2, height2) = image2.size
+
+#     result_width = width1 + width2
+#     result_height = max(height1, height2)
+
+#     result = Image.new('RGB', (result_width, result_height))
+#     result.paste(im=image1, box=(0, 0))
+#     result.paste(im=image2, box=(width1, 0))
+#     return result
+
+from cv2 import vconcat
+
+def stitch(image1, image2):
+    """give the image1 and image2 as paths of the actual files"""
+    img1 = cv2.imread(image1) 
+    img2 = cv2.imread(image2)
+    im_v = cv2.vconcat([img1, img2]) 
+    return im_v
